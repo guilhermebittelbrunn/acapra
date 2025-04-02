112edfbc6db7f07d01b5afe41278b39e784b4be6
guilhermebittelbrunn <guilherme.bitte@unifebe.edu.br>
2025-04-02 00:19:58 -0300

    ci: remove e2e tests

--- src/module/user/useCases/wishlist/wishList.module.ts
+++ src/module/user/useCases/wishlist/wishList.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common';
 
 import { CreateWishListModule } from './createWishList/createWishList.module';
-import { DeleteWishListByAnimalModule } from './deleteWishListByAnimal/deleteWishlistByAnimal.module';
+import { DeleteWishListByAnimalModule } from './deleteWishListByAnimal/deleteWishListByAnimal.module';
 
 @Module({
   imports: [CreateWishListModule, DeleteWishListByAnimalModule],
